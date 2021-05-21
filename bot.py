b22c6b34b8fc7e4c9b765854d090d89e3092a8e9
Rayr <25773909+Rayrsn@users.noreply.github.com>
2021-05-21 18:21:07 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,11 @@ from selenium.webdriver.common.keys import Keys
 import time
 import random
 
-URL = "https://www.skyroom.online/ch/TEST/webinar" # Put your URL here
-texttospam = "yes" # The text to spam goes here
+URL = input('Paste the URL: ') # Put your URL here
+texttospam = input('Text to spam: ') # The text to spam goes here
+IFTEXT = input('Enable Textspam: Y/N')
+IFHAND = input('Enable Handspam: Y/N')
+IFNAME = input('Enable Namespam: Y/N')
 
 
 profile = webdriver.FirefoxProfile()
@@ -32,35 +35,38 @@ cum4.click()
 time.sleep(1)
 
 def changename():
-	try:
-		name = ['gay','balls','sex','dababy','less gooo','cum'] # List of names to change
-		name2 = random.choice(name)
-		cum3 = driver.find_element_by_xpath('//*[@id="app_menu"]/div/button')
-		cum3.click()
-		cum4 = driver.find_element_by_xpath('/html/body/nav[1]/ul/li[1]')
-		cum4.click()
-		cum5 = driver.find_element_by_xpath('/html/body/div[5]/div[2]/table/tbody/tr[3]/td/div/div[1]/button')
-		cum5.click()
-		cum6 = driver.find_element_by_xpath('/html/body/div[5]/div[2]/table/tbody/tr[3]/td/div/div[2]/form/input')
-		cum6.clear()
-		print (name2)
-		cum6.send_keys(name2)
-		time.sleep(0.2)
-		cum7 = driver.find_element_by_xpath('/html/body/div[5]/div[2]/table/tbody/tr[3]/td/div/div[2]/form/button')
-		cum7.click()
-	except:
-		print('1')
+	if IFNAME == "Y" or IFNAME == "y":
+		try:
+			name = ['gay','balls','sex','dababy','less gooo','cum'] # List of names to change
+			name2 = random.choice(name)
+			cum3 = driver.find_element_by_xpath('//*[@id="app_menu"]/div/button')
+			cum3.click()
+			cum4 = driver.find_element_by_xpath('/html/body/nav[1]/ul/li[1]')
+			cum4.click()
+			cum5 = driver.find_element_by_xpath('/html/body/div[5]/div[2]/table/tbody/tr[3]/td/div/div[1]/button')
+			cum5.click()
+			cum6 = driver.find_element_by_xpath('/html/body/div[5]/div[2]/table/tbody/tr[3]/td/div/div[2]/form/input')
+			cum6.clear()
+			print (name2)
+			cum6.send_keys(name2)
+			time.sleep(0.2)
+			cum7 = driver.find_element_by_xpath('/html/body/div[5]/div[2]/table/tbody/tr[3]/td/div/div[2]/form/button')
+			cum7.click()
+		except:
+			print('1')
 
 def textspam():
-	#name = ['gay','balls','sex','dababy','less gooo','cum'] NOT USED
-	name2 = texttospam
-	cum8 = driver.find_element_by_xpath('//*[@id="sidebar"]/div[4]/div[2]/div/div[2]/div[3]/div[1]')
-	cum8.send_keys(name2)
-	cum8.send_keys(Keys.RETURN)
+	if IFTEXT == "Y" or IFTEXT == "y":
+		#name = ['gay','balls','sex','dababy','less gooo','cum'] NOT USED
+		name2 = texttospam
+		cum8 = driver.find_element_by_xpath('//*[@id="sidebar"]/div[4]/div[2]/div/div[2]/div[3]/div[1]')
+		cum8.send_keys(texttospam)
+		cum8.send_keys(Keys.RETURN)
 
 def handspam():
-	cum9 = driver.find_element_by_xpath('//*[@id="toolbar"]/button[7]')
-	cum9.click()
+	if IFHAND == "Y" or IFHAND == "y":
+		cum9 = driver.find_element_by_xpath('//*[@id="toolbar"]/button[7]')
+		cum9.click()
 
 # If you want to disable anything just delete it from here
 while 1 == 1:
